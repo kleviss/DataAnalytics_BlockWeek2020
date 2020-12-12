467fe5e68e11410c603112e0301ff26dfb69ffa7
Klevis Xhyra <klevissxh@gmail.com>
2020-12-12 22:03:31 +0100

    files organization

--- team_project/test.r
+++
@@ -1,6 +0,0 @@
-library(dplyr)    # alternatively, this also loads %>%
-library(ggplot2)
-
-data <- read.csv("dataset.csv", row.names = 1)
-data <- data %>% filter(PLZHalter >= 10000 & PLZHalter < 99999) %>% mutate(PLZRegion = substr(PLZHalter, 1, 1)) %>% group_by(PLZRegion) %>% summarise(claims = sum(sanz))
-ggplot(data, aes(x = PLZRegion, y = claims)) + geom_bar(stat = "identity")
